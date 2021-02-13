a13c6c311008298b199b5ec6d86b2bbdead1e0ed
Angel Sanadinov <sndnv@users.noreply.github.com>
2021-02-13 12:40:16 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+        classpath("com.android.tools.build:gradle:4.1.2")
     }
 }
 
